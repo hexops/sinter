bb44ea87fc714fd69ceae342d905b927cb8f36eb
Stephen Gutekanst <stephen@hexops.com>
2022-04-08 10:48:43 -0700

    initialize library (previously named "tridex")
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

---
+++ build.zig
@@ -0,0 +1,23 @@
+const std = @import("std");
+
+pub fn build(b: *std.build.Builder) void {
+    const mode = b.standardReleaseOptions();
+    const lib = b.addStaticLibrary("sinter", "src/main.zig");
+    lib.setBuildMode(mode);
+    lib.install();
+
+    var main_tests = b.addTest("src/main.zig");
+    main_tests.setBuildMode(mode);
+
+    const test_step = b.step("test", "Run library tests");
+    test_step.dependOn(&main_tests.step);
+}
+
+pub const pkg = std.build.Pkg{
+    .name = "sinter",
+    .path = .{ .path = thisDir() ++ "/src/main.zig" },
+};
+
+fn thisDir() []const u8 {
+    return std.fs.path.dirname(@src().file) orelse ".";
+}
