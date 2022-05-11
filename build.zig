1fb85f2fbf05ee3733fe696e0a292a13e9d1318e
Stephen Gutekanst <stephen@hexops.com>
2022-05-11 14:31:02 -0700

    add cross-compilation support to build.zig
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- build.zig
+++ build.zig
@@ -3,16 +3,20 @@ const fastfilter = @import("libs/fastfilter/build.zig");
 
 pub fn build(b: *std.build.Builder) void {
     const mode = b.standardReleaseOptions();
+    const target = b.standardTargetOptions(.{});
+
     const lib = b.addStaticLibrary("sinter", "src/libsinter.zig");
     lib.bundle_compiler_rt = true;
     lib.addPackage(fastfilter.pkg);
     lib.setBuildMode(mode);
+    lib.setTarget(target);
     lib.install();
 
     var main_tests = b.addTest("src/main.zig");
     main_tests.test_evented_io = true;
     main_tests.addPackage(fastfilter.pkg);
     main_tests.setBuildMode(mode);
+    main_tests.setTarget(target);
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
@@ -22,6 +26,7 @@ pub fn build(b: *std.build.Builder) void {
     benchmark_filter_exe.addPackage(pkg);
     benchmark_filter_exe.addPackage(fastfilter.pkg);
     benchmark_filter_exe.setBuildMode(.ReleaseFast);
+    benchmark_filter_exe.setTarget(target);
     benchmark_filter_exe.install();
 
     const benchmark_filter_run_cmd = benchmark_filter_exe.run();
